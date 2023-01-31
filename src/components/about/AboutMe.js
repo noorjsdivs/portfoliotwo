832c7ade938ffd4deb4aff3bb25eaabc6dfcda18
Noor Mohammad <noormohammad@AAOMAN.COM>
2023-01-31 16:32:52 +0400

    contact modified

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="flex flex-col lgl:flex-row pb-6">
+    <div className="flex flex-col md:flex-row pb-6">
       <div className="w-full md:w-1/2 text-zinc-400 px-6 border-r-[1px] border-r-zinc-800 flex items-center">
         <div className="py-6">
           <h2 className="font-semibold mb-1">Hello! I'm John Doe</h2>
